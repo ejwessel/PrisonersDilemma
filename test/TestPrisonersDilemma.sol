c6afd2b2ec806b7b66a69cc698830589f5b33fe8
Ethan Wessel <ejwessel@gmail.com>
2018-09-27 00:18:00 -0700

    comments for TestPrisonersDilemma

--- test/TestPrisonersDilemma.sol
+++ test/TestPrisonersDilemma.sol
@@ -7,7 +7,8 @@ import "../contracts/PrisonersDilemma.sol";
 contract TestPrisonersDilemma {
     PrisonersDilemma game = PrisonersDilemma(DeployedAddresses.PrisonersDilemma());
     
-    function testInitialStateUsingDeployedContract() public {
+    function testDeployedContractInitialState() public {
+        //After a contract is created with two addresses there should be no winner defined
         Assert.equal(address(game.winner()), address(0), "Winner address should be empty");
     }
 }
