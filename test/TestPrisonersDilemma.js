06bc24b1b83722d3137e32a4386662d2bd0ef52d
Ethan Wessel <ejwessel@gmail.com>
2018-10-14 01:17:19 -0700

    more solidity tests

--- test/TestPrisonersDilemma.js
+++ test/TestPrisonersDilemma.js
@@ -7,19 +7,58 @@ contract('Async PrisonersDilemma', async (accounts) => {
     var instance = await PrisonersDilemma.deployed();
 
     it("Test Initial State of contract", async() => {
+        //Get Player from mapping
         var player = await instance.players.call(accounts[0]);
-        assert.equal(player[0], accounts[0], "address does not match data saved");
-        assert.equal(player[1].toNumber(), CHOICES["No_Choice"], "choice does not match data saved");
-        assert.equal(player[2].toNumber(), 0, "score does not match data saved");
+        var playerAddr = player[0];
+        var playerChoice = player[1].toNumber();
+        var playerScore = player[2];
+       
+        //Test Player we expect to exist exists
+        assert.equal(playerAddr, accounts[0], `player address ${ playerAddr } is not within the contract mapping`);
+        assert.equal(playerChoice, CHOICES["No_Choice"], `player choice ${ playerChoice } does not match no choice`);
+        assert.equal(playerScore, 0, `player score ${ playerScore } does not match a score of 0`);
+
+        //Test winner is nobody
+        var contractWinner = await instance.winner();
+        assert.equal(contractWinner, EMPTY_ADDRESS, `initial contract winner should be empty`);
     });
 
-    it("Test a valid Player chooses action", async() => {
-        instance.playerChoose(CHOICES["Share"], { from: accounts[0] });
+    it("Test invalid player not in contract", async() => {
+        //Get Invalid Player from mapping
+        var player = await instance.players.call(accounts[2]);
+        var playerAddr = player[0];
+        //Invalid Player's don't have an address saved for their address
+        assert.equal(playerAddr, EMPTY_ADDRESS, `player address ${ playerAddr } was valid and should not be`);
+    });
+
+    it("Test valid Player chooses action", async() => {
+        //Player chooses Share
+        await instance.playerChoose(CHOICES["Share"], { from: accounts[0] });
+        //Get Player from mapping
         var player = await instance.players.call(accounts[0]);
-        assert.equal(player[1].toNumber(), CHOICES["Share"], "choice does not match data saved");
+        var playerChoice = player[1].toNumber();
+        assert.equal(playerChoice, CHOICES["Share"], `player choice ${ CHOICES["SHARE"] } was not recorded`);
+    });
+
+    it("Test invalid Player chooses action", async() => {
+        try {
+            await instance.playerChoose(CHOICES["Share"], { from: accounts[2] });
+        } catch (error) {
+            console.log(error);
+            console.log("error was caught");
+        }
     });
 
 });
+
+
+
+
+
+
+
+
+
 
 contract('PrisonersDilemma', function(accounts) {
     it("Test: initialize the contract with no winner", function() {
