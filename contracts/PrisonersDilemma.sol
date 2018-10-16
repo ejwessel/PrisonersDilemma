00fa5d2ae3c2b70591d4b6f4f3d1bfac2ff8a062
Ethan Wessel <ejwessel@gmail.com>
2018-10-15 23:58:39 -0700

    remove unnecessary comment

--- contracts/PrisonersDilemma.sol
+++ contracts/PrisonersDilemma.sol
@@ -77,6 +77,4 @@ contract PrisonersDilemma {
         require(players[playerAddr].addr != address(0), "Player address is not in contract");
         return players[playerAddr].score;
     }
-
-    //function to get winner
 }
