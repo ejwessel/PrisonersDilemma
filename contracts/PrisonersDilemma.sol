c0627980ed0b67d330fb0ec01a9f34fdd19981d1
Ethan Wessel <ejwessel@gmail.com>
2018-10-25 21:25:41 -0700

    make logic a bit cleaner to look at

--- contracts/PrisonersDilemma.sol
+++ contracts/PrisonersDilemma.sol
@@ -1,27 +1,11 @@
 pragma solidity ^0.4.24;
 
-/**
-This contract mimics that of the prisoners dilemma
-
-+----------+----------------------+
-|          |       Player 2       |
-+----------+----------------------+
-|          |       | Share | Take |
-+          +-------+-------+------+
-| Player 1 | Share | 1/1   | 5/0  |
-+          +-------+-------+------+
-|          | Take  | 0/5   | 0/0  |
-+----------+-------+-------+------+
-Rules:
-- If two places 'share' they both receive 1 point and the game progresses
-- If two players 'take', they receive no points and the game doesn't progress
-- If either one of the players takes while the other player shares, the person who shared will receive no points
-- Game ends when one of the players gets 20 points
-- Ties are possible
-**/
-
 contract PrisonersDilemma {
 
+    //Constants
+    uint constant private WINNING_SCORE = 20;
+    uint constant private GREEDY_POINTS = 5;
+    uint constant private MUTUAL_POINTS = 1;
     enum ActionChoices { NoChoice, Share, Take }
 
     struct Player {
@@ -34,7 +18,6 @@ contract PrisonersDilemma {
     address[] private playerList;
     mapping (address => Player) public players;
     address public winner = address(0); //empty address
-    uint constant private WINNING_SCORE = 20;
 
     //Events
     event ContractInitialized(address _player1, address _player2);
@@ -61,6 +44,9 @@ contract PrisonersDilemma {
         //0 is default value. We aren't going to allow default values
         require(players[msg.sender].addr != address(0), "Player address is not in contract");
 
+        //Game stops once a winner has been declared
+        require(winner == address(0), "A winner or tie has been declared");
+
         //Require player to not pass 'NoChoice'
         require(choice != ActionChoices.NoChoice, "No selection made, player chose No Choice");
 
@@ -68,63 +54,59 @@ contract PrisonersDilemma {
         //Otherwise the player has already made a choice
         require(players[msg.sender].choice == ActionChoices.NoChoice, "Player already made a choice");
       
-        //TODO: need a condition to prevent players from playing after a winner is found
-
         //Set Player Choice 
         players[msg.sender].choice = choice;
-
         emit PlayerSelectedChoice(msg.sender);
 
-        handlePlayerChoice();
+        tallyPlayerScores();
+        checkForWinner();
     }
 
-    //function to handle after a player chooses
-    function handlePlayerChoice() private {
-        for (uint i = 0; i < playerList.length; i++) {
-            //get opponent address
-            address opponentAddr = playerList[i];
-            //if the opponent is the current player ignore
-            if(opponentAddr == msg.sender) {
-                continue;
-            }
-            //get the opponent
-            Player memory opponent = players[opponentAddr];
-            //check if the opponent has made a choice yet
-            if( opponent.choice == ActionChoices.NoChoice ) {
-                return;
-            }
-            
-            //get the current player
-            Player memory currentPlayer = players[msg.sender];
-            
-            //handle scoring logic
-            if(opponent.choice == ActionChoices.Take && currentPlayer.choice == ActionChoices.Take) {
-                return;
-            } else if (opponent.choice == ActionChoices.Share && currentPlayer.choice == ActionChoices.Take) {
-                currentPlayer.score += 5;
-            } else if (opponent.choice == ActionChoices.Take && currentPlayer.choice == ActionChoices.Share) {
-                opponent.score += 5;
-            } else {
-                currentPlayer.score += 1;
-                opponent.score += 1;
-            }
+    function tallyPlayerScores() private {
+        //get players
+        Player memory player1 = players[playerList[0]];
+        Player memory player2 = players[playerList[1]];
 
-            //after scoring check if there is a winner and set winner
-            if(currentPlayer.score >= WINNING_SCORE || opponent.score >= WINNING_SCORE){
-                if(currentPlayer.score >= WINNING_SCORE && opponent.score == WINNING_SCORE){
-                    //no winner
-                    return;
-                } else if (currentPlayer.score >= WINNING_SCORE) {
-                    //current player winner
-                    winner = currentPlayer.addr;
-                } else {
-                    //opponent winner
-                    winner = opponent.addr;
-                }
-                emit AlertWinner(winner);
-            }
-            //implicit else do nothing if nobody has won
+        //check either player hasn't made a choice and exit
+        if ((player1.choice == ActionChoices.NoChoice) || (player2.choice == ActionChoices.NoChoice)) {
+            return;
         }
+
+        //tally player scores
+        if(player1.choice == ActionChoices.Take && player2.choice == ActionChoices.Take) {
+            //both players are awarded 0 points
+            return;
+        } else if (player1.choice == ActionChoices.Share && player2.choice == ActionChoices.Take) {
+            player2.score += GREEDY_POINTS;
+        } else if (player1.choice == ActionChoices.Take && player2.choice == ActionChoices.Share) {
+            player1.score += GREEDY_POINTS;
+        } else {
+            player2.score += MUTUAL_POINTS;
+            player1.score += MUTUAL_POINTS;
+        }
+
+        //reset player choices for next round
+        player1.choice = ActionChoices.NoChoice;
+        player2.choice = ActionChoices.NoChoice;
+    }
+
+    function checkForWinner() private {
+        //get players
+        Player memory player1 = players[playerList[0]];
+        Player memory player2 = players[playerList[1]];
+        
+        if (player1.score < WINNING_SCORE && player2.score < WINNING_SCORE) {
+            //no winner keep playing
+            return;
+        } else if(player1.score >= WINNING_SCORE && player2.score >= WINNING_SCORE) {
+            //there is no winner, set the winner as the contract
+            winner = this;
+        } else if (player1.score >= WINNING_SCORE) {
+            winner = player1.addr;
+        } else {
+            winner = player2.addr;
+        } 
+        emit AlertWinner(winner);
     }
 
     //function to get a player's scores
