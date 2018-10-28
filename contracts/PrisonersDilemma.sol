cb5868f7c0890461bc13f5cb08da10b030477af0
Ethan Wessel <ejwessel@gmail.com>
2018-10-28 13:10:35 -0700

    Scoring and Winner Logic (#7)
    * Scoring and Winner Logic
    
    * make logic a bit cleaner to look at
    
    * add scoring test
    
    * don't need to deploy migration 2 when handled within tests
    
    * getting tests set up for scoring and winner
    
    * basic tests, more to come
    
    * add some more tests and reduce some left over code
    
    * add winner check and test for correct winner
    
    * shorten test
    
    * add tie test
    
    * comment
    
    * reduce step test
    
    * fix comment
    
    * clarifying comments
    
    * fixed a bug and added additional tests
    
    * add additioanl assertions
    
    * Typos

--- contracts/PrisonersDilemma.sol
+++ contracts/PrisonersDilemma.sol
@@ -1,27 +1,11 @@
 pragma solidity ^0.4.24;
 
-/**
-This contract mimics that of the prisoners dilemma
-
-+----------+----------------------+
-|          |       Player 2       |
-+----------+----------------------+
-|          |       | Share | Take |
-+          +-------+-------+------+
-| Player 1 | Share | 1/1   | 5/0  |
-+          +-------+-------+------+
-|          | Take  | 0/5   | 0/0  |
-+----------+-------+-------+------+
-Rules:
-- If two places 'share' they both receive 1 point and the game progresses
-- If two players 'take', they receive no points and the game doesn't progress
-- If either one of the players takes while the other player shares, the person who shared will receive no points
-- Game ends when one of the players gets 20 points
-- Ties are possible
-**/
-
 contract PrisonersDilemma {
 
+    //Constants
+    uint constant private WINNING_SCORE = 20;
+    uint constant private GREEDY_POINTS = 5;
+    uint constant private MUTUAL_POINTS = 1;
     enum ActionChoices { NoChoice, Share, Take }
 
     struct Player {
@@ -31,21 +15,24 @@ contract PrisonersDilemma {
     }
 
     //State Variables
+    address[] private playerList;
     mapping (address => Player) public players;
     address public winner = address(0); //empty address
 
     //Events
     event ContractInitialized(address _player1, address _player2);
     event PlayerSelectedChoice(address _player);
+    event PlayersScoresTallied();
     event AlertWinner(address _player);
 
     constructor(address _player1, address _player2) public {
 
-        Player memory player1 = Player(_player1, ActionChoices.NoChoice, 0);
-        Player memory player2 = Player(_player2, ActionChoices.NoChoice, 0);
+        players[_player1] = Player(_player1, ActionChoices.NoChoice, 0);
+        playerList.push(_player1);
+        
+        players[_player2] = Player(_player2, ActionChoices.NoChoice, 0);
+        playerList.push(_player2);
 
-        players[_player1].addr = _player1;
-        players[_player2].addr = _player2;
         emit ContractInitialized(_player1, _player2);
     }
 
@@ -57,19 +44,70 @@ contract PrisonersDilemma {
         //0 is default value. We aren't going to allow default values
         require(players[msg.sender].addr != address(0), "Player address is not in contract");
 
+        //Game stops once a winner has been declared
+        require(winner == address(0), "A winner or tie has been declared");
+
         //Require player to not pass 'NoChoice'
         require(choice != ActionChoices.NoChoice, "No selection made, player chose No Choice");
 
         //Update player choice iff the existing state is ActionChoices.NoChoice
         //Otherwise the player has already made a choice
         require(players[msg.sender].choice == ActionChoices.NoChoice, "Player already made a choice");
-       
+      
         //Set Player Choice 
         players[msg.sender].choice = choice;
-
         emit PlayerSelectedChoice(msg.sender);
 
-        //TOOD: I need to add logic here that compares the selected choice of this user and the other user
+        tallyPlayerScores();
+        checkForWinner();
+    }
+
+    function tallyPlayerScores() private {
+        //get players
+        Player storage player1 = players[playerList[0]];
+        Player storage player2 = players[playerList[1]];
+
+        //check either player hasn't made a choice and exit
+        if ((player1.choice == ActionChoices.NoChoice) || (player2.choice == ActionChoices.NoChoice)) {
+            //I don't require here because require would refund gas
+            return;
+        }
+
+        //tally player scores
+        if(player1.choice == ActionChoices.Share && player2.choice == ActionChoices.Share){
+            player2.score += MUTUAL_POINTS;
+            player1.score += MUTUAL_POINTS;
+        } else if (player1.choice == ActionChoices.Share && player2.choice == ActionChoices.Take) {
+            player2.score += GREEDY_POINTS;
+        } else if (player1.choice == ActionChoices.Take && player2.choice == ActionChoices.Share) {
+            player1.score += GREEDY_POINTS;
+        }
+        //Implicit else both players are awarded 0 points
+
+        //reset player choices for next round
+        player1.choice = ActionChoices.NoChoice;
+        player2.choice = ActionChoices.NoChoice;
+
+        emit PlayersScoresTallied();
+    }
+
+    function checkForWinner() private {
+        //get players
+        Player storage player1 = players[playerList[0]];
+        Player storage player2 = players[playerList[1]];
+        
+        if (player1.score < WINNING_SCORE && player2.score < WINNING_SCORE) {
+            //no winner keep playing
+            return;
+        } else if(player1.score >= WINNING_SCORE && player2.score >= WINNING_SCORE) {
+            //there is no winner, set the winner as the contract
+            winner = this;
+        } else if (player1.score >= WINNING_SCORE) {
+            winner = player1.addr;
+        } else {
+            winner = player2.addr;
+        } 
+        emit AlertWinner(winner);
     }
 
     //function to get a player's scores
--- test/TestPrisonersDilemma.js
+++ test/TestPrisonersDilemma.js
@@ -6,8 +6,8 @@ var EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 contract('PrisonersDilemma', async (accounts) => {
 
-    before(async() => {
-        instance = await PrisonersDilemma.deployed();
+    beforeEach(async() => {
+        instance = await PrisonersDilemma.new(accounts[0], accounts[1]);
     });
 
     it("Test Initial State of contract", async() => {
@@ -47,14 +47,14 @@ contract('PrisonersDilemma', async (accounts) => {
         //Test invalid Player chooses action share
         await expectThrow(instance.playerChoose(CHOICES["Share"], { from: accounts[2] }));
 
+        //Test player cannot pass an invalid choice
+        await expectThrow(instance.playerChoose(9, { from: accounts[0] }));
+
         //Test player chooses NoChoice
         await expectThrow(instance.playerChoose(CHOICES["NoChoice"], { from: accounts[0] }));
 
         //Test player cannot update choice if choice is already made
         await expectThrow(instance.playerChoose(CHOICES["Take"], { from: accounts[0] }));
-
-        //Test player cannot pass an invalid choice
-        await expectThrow(instance.playerChoose(9, { from: accounts[0] }));
     });
 
     it("Test getPlayerScore()", async() => {
@@ -64,5 +64,82 @@ contract('PrisonersDilemma', async (accounts) => {
         //Test that a player's score can be retrieved        
         var playerScore = await instance.getPlayerScore(accounts[0], { from: accounts[0] });
         assert.equal(playerScore, 0, `player score ${ playerScore } does not match a score of 0`);
+    });
+    
+    it("Test scoring with 1 winner", async() => {
+        //Round 1
+        await instance.playerChoose(CHOICES["Share"], { from: accounts[0] });
+        await instance.playerChoose(CHOICES["Share"], { from: accounts[1] });
+
+        for(i = 0; i < 4; i++) {
+            await instance.playerChoose(CHOICES["Share"], { from: accounts[0] });
+            await instance.playerChoose(CHOICES["Take"], { from: accounts[1] });
+        }
+
+        var player1 = await instance.players.call(accounts[0]);
+        var player2 = await instance.players.call(accounts[1]);
+    
+        //both player choices should be reset
+        assert.equal(player1[1].toNumber(), 0, "Player 1 choice was not reset");
+        assert.equal(player2[1].toNumber(), 0, "Player 2 choice was not reset");
+
+        //there should have been a winner
+        var player1Score = await instance.getPlayerScore(accounts[0]);
+        var player2Score = await instance.getPlayerScore(accounts[1]);
+        assert.equal(player1Score, 1, `Player 1 points should be 1, not ${ player1Score }`);
+        assert.equal(player2Score, 21, `Player 2 points should be 21, not ${ player2Score }`);
+
+        var contractWinner = await instance.winner();
+        assert.equal(contractWinner, accounts[1], `contract winner ${ contractWinner }, does not match expected ${ accounts[1] }`);
+
+        //Test if a player can continue to play after there is a winner
+        await expectThrow(instance.playerChoose(CHOICES["Take"], { from: accounts[0] }))
+    });
+    
+    it("Test scoring with no winner", async() => {
+
+        //create 20 rounds
+        for(i = 0; i < 20; i++) {
+            await instance.playerChoose(CHOICES["Share"], { from: accounts[0] });
+            await instance.playerChoose(CHOICES["Share"], { from: accounts[1] });
+        }
+
+        var player1 = await instance.players.call(accounts[0]);
+        var player2 = await instance.players.call(accounts[1]);
+    
+        //both player choices should be reset
+        assert.equal(player1[1].toNumber(), 0, "Player 1 choice was not reset");
+        assert.equal(player2[1].toNumber(), 0, "Player 2 choice was not reset");
+
+        //there should be no winner
+        var player1Score = await instance.getPlayerScore(accounts[0]);
+        var player2Score = await instance.getPlayerScore(accounts[1]);
+        assert.equal(player1Score, 20, `Player 1 points should be 20, not ${ player1Score }`);
+        assert.equal(player2Score, 20, `Player 2 points should be 20, not ${ player2Score }`);
+
+        var contractWinner = await instance.winner();
+        assert.equal(contractWinner, instance.address, `contract winner ${ contractWinner }, does not match expected ${ instance.address }`);
+    });
+
+    it("Test scoring with no progression", async() => {
+
+        //create 2 rounds
+        for(i = 0; i < 2; i++) {
+            await instance.playerChoose(CHOICES["Take"], { from: accounts[0] });
+            await instance.playerChoose(CHOICES["Take"], { from: accounts[1] });
+        }
+
+        var player1 = await instance.players.call(accounts[0]);
+        var player2 = await instance.players.call(accounts[1]);
+    
+        //both player choices should be reset
+        assert.equal(player1[1].toNumber(), 0, "Player 1 choice was not reset");
+        assert.equal(player2[1].toNumber(), 0, "Player 2 choice was not reset");
+
+        //there should be no winner
+        var player1Score = await instance.getPlayerScore(accounts[0]);
+        var player2Score = await instance.getPlayerScore(accounts[1]);
+        assert.equal(player1Score, 0, `Player 1 points should be 0, not ${ player1Score }`);
+        assert.equal(player2Score, 0, `Player 2 points should be 0, not ${ player2Score }`);
     });
 });
