616807ccf44fba211454a3becf62a94b16ebac59
Ethan Wessel <ejwessel@gmail.com>
2019-01-21 11:02:16 -0800

    scoreboard components

---
+++ web-app/src/GameScoreboardComponents/GameScoreboardComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GameScoreboardComponent extends Component {
+  render() {
+    return (
+      <h1>GameScoreboardComponent</h1>
+    );
+  }
+}
+
+export default GameScoreboardComponent;
---
+++ web-app/src/GameScoreboardComponents/GameScoreboardPlayerScoreComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GameScoreboardPlayerScoreComponent extends Component {
+  render() {
+    return (
+      <h1>GameScoreboardPlayerScoreComponent</h1>
+    );
+  }
+}
+
+export default GameScoreboardPlayerScoreComponent;
