c311428d70012b5ffe60ccaed4b465ef49ffe451
brian <bchou515@gmail.com>
2019-05-04 13:30:43 -0400

    renamed files

--- web-app/src/GameScoreboardComponents/tests/GameScoreboardPlayerScoreComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GameScoreboardPlayerScoreComponent from '../GameScoreboardPlayerScoreComponent';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GameScoreboardPlayerScoreComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- web-app/src/GameTurnsComponents/tests/GameTurnsPlayerChoiceComponent.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GameTurnsPlayerChoiceComponent from '../GameTurnsPlayerChoiceComponent';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<GameTurnsPlayerChoiceComponent />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
