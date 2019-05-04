d8cfdd306ab1da41dc9c16d0c14743857ca33b96
Ethan Wessel <ejwessel@gmail.com>
2019-05-04 14:20:52 -0400

    fix index

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import GameComponent from './GameComponent';
+import Game from './Game';
 
-ReactDOM.render(<GameComponent />, document.getElementById('root'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
