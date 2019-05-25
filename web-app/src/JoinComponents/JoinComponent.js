4e2219f765f76a384f9e748be7873bf6333e87ab
Ethan Wessel <ejwessel@gmail.com>
2019-05-25 14:43:03 -0400

    join existing game

--- web-app/src/JoinComponents/JoinComponent.js
+++ web-app/src/JoinComponents/JoinComponent.js
@@ -2,22 +2,45 @@ import React, { Component } from 'react';
 
 class JoinComponent extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contractAddress: '0x0000000000000000000000000000000000000000'
+    }
+
+    this.handleContractAddress = this.handleContractAddress.bind(this);
+    this.handleStartGame = this.handleStartGame.bind(this)
+  }
+
+  handleContractAddress(event) {
+    this.setState({contractAddress: event.target.value}) 
+  }
+
+  handleStartGame(event) {
+    event.preventDefault();
+    this.props.setContract(this.state.contractAddress);
+  }
+
   render() {
     return (
-      <form>
-        <label>Contract Address:</label>
-        {' '}
-        <input
-          type = "text"
-          contract_address = ""
-        />
-        {' '}
-        <input
-          type = "button"
-          value = "Join Game"
-          onClick = { this.submitForm }
-        />
-      </form>
+      <div>
+        <form onSubmit={ this.handleStartGame }>
+          <label>Contract Address:</label>
+          {' '}
+          <input
+            type = "text"
+            name = "contractAddress"
+            placeholder = "Contact Address"
+            onChange = { this.handleContractAddress }
+          />
+          <br/>
+          <input
+            type = "submit"
+            value = "Join Game"
+          />
+        </form>
+      </div>
     );
   }
 }
