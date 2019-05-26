2b2ffdde86a6416c27cd760363e1b625a973a0e1
Ethan Wessel <ejwessel@gmail.com>
2019-05-25 20:59:36 -0400

    Updates

---
+++ precache-manifest.8550872172a6109a290b3b6e2fb3969c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aff97779a22357290e9b",
+    "url": "/PrisonersDilemma/static/css/main.fd419304.chunk.css"
+  },
+  {
+    "revision": "aff97779a22357290e9b",
+    "url": "/PrisonersDilemma/static/js/main.81232eb0.chunk.js"
+  },
+  {
+    "revision": "a0bcf8c1716fdf1253ad",
+    "url": "/PrisonersDilemma/static/js/runtime~main.5a8c9073.js"
+  },
+  {
+    "revision": "b1ab9ec0c81034d549a8",
+    "url": "/PrisonersDilemma/static/js/2.6cd9e813.chunk.js"
+  },
+  {
+    "revision": "fd6857d5b44434042c8db5abc82dd3f0",
+    "url": "/PrisonersDilemma/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/PrisonersDilemma/precache-manifest.8550872172a6109a290b3b6e2fb3969c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/PrisonersDilemma/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
